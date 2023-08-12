ae09bd5a2c992c2e1fa66bbe8937990cf74697b4
Jonathan Wang <jonathan.wang1996@gmail.com>
2023-08-12 13:44:07 -0500

    comment out fetches

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,11 @@ import { fetchPageInfo, fetchExperiences, fetchProjects, fetchSkills, fetchSocia
 import { ArrowSmallUpIcon } from "@heroicons/react/24/solid";
 
 export default async function Home() {
-  const experiences: Experience[] = await fetchExperiences();
-  const pageInfo: PageInfo = await fetchPageInfo();
-  const projects: Project[] = await fetchProjects();
-  const skills: Skill[] = await fetchSkills();
-  const socials: Social[] = await fetchSocials();
+  // const experiences: Experience[] = await fetchExperiences();
+  // const pageInfo: PageInfo = await fetchPageInfo();
+  // const projects: Project[] = await fetchProjects();
+  // const skills: Skill[] = await fetchSkills();
+  // const socials: Social[] = await fetchSocials();
 
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-accent-80">
