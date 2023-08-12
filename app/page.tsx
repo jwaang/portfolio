dd7528122af15fdb64a6be2803a1f6a9b8ba530b
Jonathan Wang <jonathan.wang1996@gmail.com>
2023-08-12 13:23:59 -0500

    Move api folder to app and remove pages

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
 
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-accent-80">
-      <Header socials={socials} />
+      <Header socials={socials || []} />
 
       <section id="hero" className="snap-start">
         <Hero pageInfo={pageInfo} />
